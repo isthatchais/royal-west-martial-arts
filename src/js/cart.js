8c6244ea6a4caaafbf3a1ef165810f0d1c301692
Chais Chang <cha96001@byui.edu>
2023-03-18 22:56:58 -0600

    wip

---
+++ src/js/cart.js
@@ -0,0 +1,71 @@
+
+import {
+    getLocalStorage,
+    setLocalStorage, 
+    setClick
+} from './utils.mjs';
+
+function renderCartContents() {
+    const cartItems = getLocalStorage('so-cart');
+    let htmlItems = '';
+    if (cartItems != null) {
+        htmlItems = cartItems.map((item) => cartItemTemplate(item));
+        document.querySelector('.product-list').innerHTML = htmlItems.join('');
+        document.querySelectorAll('.cart-card__delete').forEach(function (item) {
+            setClick(`#${item.getAttribute('id')}`,deleteCartItem)
+        });
+    } else document.querySelector('.product-list').innerHTML = '';
+}
+
+function cartItemTemplate(item) {
+    const newItem = `<li class='cart-card divider'>
+  <a href='#' class='cart-card__image'>
+    <img
+      src='${item.Image}'
+      alt='${item.Name}'
+    />
+  </a>
+  <a href='#'>
+    <h2 class='card__name'>${item.Name}</h2>
+  </a>
+  <p class='cart-card__color'>${item.Colors[0].ColorName}</p>
+  <p class='cart-card__quantity'>qty: ${item.Quantity}</p>
+  <p class='cart-card__price'>$${item.FinalPrice}</p>
+  <button id='deleteButton_${item.Id}' data-id='${item.Id}' class='cart-card__delete'>X</button>
+  
+</li>`;
+
+    return newItem;
+}
+
+function displayTotal() {
+    const cartItems = getLocalStorage('so-cart');
+    const totalSection = document.querySelector('.total');
+    const totalSum = document.getElementById('total-sum');
+    if (cartItems != null) {
+        totalSection.classList.add('show');
+        let total = 0,
+            i = 0;
+        for (i = 0; i < cartItems.length; i++) {
+            total += parseFloat(cartItems[i].FinalPrice);
+        }
+        totalSum.innerText = Math.round(total * 100) / 100;
+    } else {
+        totalSection.classList.remove('show');
+    }
+}
+
+function deleteCartItem() {
+    const cartItems = getLocalStorage('so-cart');
+    const index = cartItems.findIndex(obj => obj.Id == this.getAttribute('data-id'));
+    if (index >= 0) {
+        cartItems.splice(index, 1);
+        setLocalStorage('so-cart', cartItems);
+        renderCartContents();
+        displayTotal();
+        displaySuperscriptNumber();
+    }
+}
+
+displayTotal();
+renderCartContents();
