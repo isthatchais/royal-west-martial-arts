9298eef20b2cbfb367b08009f0d86a50db69b81c
Chais Chang <cha96001@byui.edu>
2023-03-23 17:44:51 -0600

    fixing vite config

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
                 main: resolve(__dirname, 'src/index.html'),
                 cart: resolve(__dirname, 'src/cart/index.html'),
                 checkout: resolve(__dirname, 'src/checkout/index.html'),
-                product1: resolve(__dirname, 'src/product_pages/index.html'),
+                program1: resolve(__dirname, 'src/program-description/index.html'),
                 productListing: resolve(__dirname, 'src/product-listing/index.html'),
                 admin: resolve(__dirname, 'src/admin/index.html'),
             },
