38edf6c1f4a2af4b736169f024398d155bf2d6b8
Chais Chang <cha96001@byui.edu>
2023-03-25 13:57:43 -0600

    try again

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,6 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'src/index.html'),
-                checkout: resolve(__dirname, 'src/checkout/index.html'),
                 program1: resolve(__dirname, 'src/program-description/index.html'),
                 formListing: resolve(__dirname, 'src/form-listing/index.html'),
                 formPages: resolve(__dirname, 'src/form-pages/index.html'),
